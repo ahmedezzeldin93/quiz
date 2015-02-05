52a3bbdaf34f4534fcf3c303b5c76b0068a6f363
Ahmed Ezz Eldin <ahmedezeldin93@gmail.com>
2015-02-05 07:40:59 +0200

    Setting Welcoming

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  namespace :api, defaults: { format: :json } do
-    
-  end
+	
+	get 'welcome/index'
+	root 'welcome#index'
+	get 'api', to: 'welcome#index'
+
+	devise_for :users
+	
+	namespace :api, defaults: { format: :json } do
+  		resources :users 
+  	end
+
 end

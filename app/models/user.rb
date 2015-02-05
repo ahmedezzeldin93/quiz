98897372b2dcd81933195e17526451e28ae3aba6
Ahmed Ezz Eldin <ahmedezeldin93@gmail.com>
2015-02-05 07:01:37 +0200

    Add validation to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+    USERNAME_REGEX = /[a-zA-Z](([\._\-][a-zA-Z0-9])|[a-zA-Z0-9])*[a-z0-9]/
+    NAME_REGEX = /[a-zA-Z]/
+	
+	validates :username, 
+	presence: true,
+	length: { minimum: 5, maximum: 25},
+	uniqueness: true,
+	format: USERNAME_REGEX
+
+
+	validates :first_name, presence: true,format: NAME_REGEX
+	validates :last_name, presence: true,format: NAME_REGEX
+	validates :type, presence: true
 end
